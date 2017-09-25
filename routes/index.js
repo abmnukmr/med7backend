54236a8d42b2bd417d10cc561125db8fb36baa13
abmnukmr <developer.abmnu@gmail.com>
2017-09-25 17:25:24 +0530

    snacks time 250917

--- routes/index.js
+++ routes/index.js
@@ -64,57 +64,71 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// add a doctor
+//////// add a doctor
+///////
+router.get('/add/doctor/:email',function (req,res,next) {
+  doctor.find({"email":req.params.email},function (err,docs) {
 
-router.get('/add/doctor',function (req,res,next) {
-    var d=new Date();
-    var m=("0" + (d.getMonth() + 1)).slice(-2);
-    var y=d.getFullYear();
-    var d=("0" + (d.getDate() )).slice(-2);
+    // res.json(docs.length)
 
-    var item={
-        "name":"asbkas",
-        "email":"",
-        "exper":"",
-        "fee":"",
-        "lic_no":" ",
-        "spl":"",
-        "date":y.toString()+m.toString()+d.toString(),
-        "address":"",
-        "lat":"",
-        "show":"false",
-        "lng":"",
-        "phone":"",
-        "sun_time":"",
-        "mon_time":"",
-        "tue_time":"",
-        "wed_time":"",
-        "thu_time":"",
-        "fri_time":"",
-        "sat_time":"",
-       }
-    doctor_search.update({"search":"gogolio"},{$push:{"docters":item}},false ,
-        true,function (req,res) {
+    if(docs.length<1){
 
+        var d = new Date();
+        var m = ("0" + (d.getMonth() + 1)).slice(-2);
+        var y = d.getFullYear();
+        var d = ("0" + (d.getDate() )).slice(-2);
 
-      if(err){
-        console.log(err)
-      }
-      else  console.log("sucesssfully added doctor_search")
-
-      })
-
-    doctor.insert(item,function (req,res) {
-        if(err){
-            console.log(err)
+        var item = {
+            "name": "asbkas",
+            "email": req.params.email,
+            "exper": "",
+            "fee": "",
+            "lic_no": " ",
+            "spl": "",
+            "date": y.toString() + m.toString() + d.toString(),
+            "address": "",
+            "lat": "",
+            "show": "false",
+            "lng": "",
+            "phone": "",
+            "sun_time": "",
+            "mon_time": "",
+            "tue_time": "",
+            "wed_time": "",
+            "thu_time": "",
+            "fri_time": "",
+            "sat_time": "",
         }
-        else  console.log("sucesssfully added doctor")
-
-    })
-
+        doctor_search.update({"search": "gogolio"}, {$push: {"docters": item}}, false,
+            true, function (req, res) {
 
 
-    res.send("insertion successfully");
+                if (err) {
+                    console.log(err)
+                }
+                else console.log("sucesssfully added doctor_search")
+
+            })
+
+        doctor.insert(item, function (req, res) {
+            if (err) {
+                console.log(err)
+            }
+            else console.log("sucesssfully added doctor")
+
+        })
+
+        res.send("insertion successfully");
+
+    }
+    else {
+      res.send("error")
+    }
+
+     // var  ty=json(docs.length)
+  });
+
+
 
 })
 
@@ -123,60 +137,83 @@ router.get('/add/doctor',function (req,res,next) {
 
 ////add a hospital
 
-router.get('/add/hospital',function (req,res,next) {
-    var d=new Date();
-    var m=("0" + (d.getMonth() + 1)).slice(-2);
-    var y=d.getFullYear();
-    var d=("0" + (d.getDate() )).slice(-2);
-
-    var item={
-        "name":"asbkas",
-        "email":"",
-        "doctors":"",
-        "date":y.toString()+m.toString()+d.toString(),
-        "address":"",
-        "lat":"",
-        "lng":"",
-        "show":"false",
-        "phone":"",
-         "services":""
-    }
-    hospital_search.update({"search":"gogolio"},{$push:{"docters":item}},false ,
-        true,function (req,res) {
+router.get('/add/hospital/:email',function (req,res,next) {
 
 
-            if(err){
-                console.log(err)
+    hospital.find({"email":req.params.email},function (err,docs) {
+
+        // res.json(docs.length)
+
+        if(docs.length<1) {
+
+
+            var d = new Date();
+            var m = ("0" + (d.getMonth() + 1)).slice(-2);
+            var y = d.getFullYear();
+            var d = ("0" + (d.getDate() )).slice(-2);
+
+            var item = {
+                "name": "asbkas",
+                "email": req.params.email,
+                "doctors": "",
+                "date": y.toString() + m.toString() + d.toString(),
+                "address": "",
+                "lat": "",
+                "lng": "",
+                "show": "false",
+                "phone": "",
+                "services": ""
             }
-            else  console.log("sucesssfully added doctor_search")
+            hospital_search.update({"search": "gogolio"}, {$push: {"docters": item}}, false,
+                true, function (req, res) {
 
-        })
 
-    hospital.insert(item,function (req,res) {
-        if(err){
-            console.log(err)
+                    if (err) {
+                        console.log(err)
+                    }
+                    else console.log("sucesssfully added doctor_search")
+
+                })
+
+            hospital.insert(item, function (req, res) {
+                if (err) {
+                    console.log(err)
+                }
+                else console.log("sucesssfully added doctor")
+
+            })
+
+            res.send("insertion successfully");
+
+
         }
-        else  console.log("sucesssfully added doctor")
+        else {
+                res.send("error")
+            }
+        });
 
-    })
 
-
-
-    res.send("insertion successfully");
 
 })
-
 //// add a chemist
 
-router.get('/add/chemist',function (req,res,next) {
-    var d=new Date();
+router.get('/add/chemist/:email',function (req,res,next) {
+
+
+    chemist.find({"email":req.params.email},function (err,docs) {
+
+        // res.json(docs.length)
+
+        if(docs.length<1) {
+
+            var d=new Date();
     var m=("0" + (d.getMonth() + 1)).slice(-2);
     var y=d.getFullYear();
     var d=("0" + (d.getDate() )).slice(-2);
 
     var item={
         "name":"asbkas",
-        "email":"",
+        "email":req.params.email,
         "date":y.toString()+m.toString()+d.toString(),
         "address":"",
         "lat":"",
@@ -205,24 +242,41 @@ router.get('/add/chemist',function (req,res,next) {
     })
 
 
-
     res.send("insertion successfully");
+
+}
+else {
+    res.send("error")
+}
+
+// var  ty=json(docs.length)
+});
+
+
 
 })
 
 
-
 //// add a Ayurveda
 
-router.get('/add/ayurveda',function (req,res,next) {
-    var d=new Date();
+router.get('/add/ayurveda/:email',function (req,res,next) {
+
+
+    ayurveda.find({"email":req.params.email},function (err,docs) {
+
+        // res.json(docs.length)
+
+        if(docs.length<1) {
+
+
+            var d=new Date();
     var m=("0" + (d.getMonth() + 1)).slice(-2);
     var y=d.getFullYear();
     var d=("0" + (d.getDate() )).slice(-2);
 
     var item={
         "name":"asbkas",
-        "email":"",
+        "email":req.params.email,
         "date":y.toString()+m.toString()+d.toString(),
         "address":"",
         "lat":"",
@@ -250,24 +304,39 @@ router.get('/add/ayurveda',function (req,res,next) {
 
     })
 
+            res.send("insertion successfully");
+
+        }
+        else {
+            res.send("error")
+        }
+
+        // var  ty=json(docs.length)
+    });
 
 
-    res.send("insertion successfully");
 
 })
 
-
 /////add homeo
 
-router.get('/add/homeo',function (req,res,next) {
-    var d=new Date();
+router.get('/add/homeo/:email',function (req,res,next) {
+
+
+    homeo.find({"email":req.params.email},function (err,docs) {
+
+        // res.json(docs.length)
+
+        if(docs.length<1) {
+
+            var d=new Date();
     var m=("0" + (d.getMonth() + 1)).slice(-2);
     var y=d.getFullYear();
     var d=("0" + (d.getDate() )).slice(-2);
 
     var item={
         "name":"asbkas",
-        "email":"",
+        "email":req.params.email,
         "date":y.toString()+m.toString()+d.toString(),
         "address":"",
         "lat":"",
@@ -296,23 +365,39 @@ router.get('/add/homeo',function (req,res,next) {
     })
 
 
+            res.send("insertion successfully");
 
-    res.send("insertion successfully");
+        }
+        else {
+            res.send("error")
+        }
+
+        // var  ty=json(docs.length)
+    });
+
+
 
 })
 
-
 //// add a pathalogy
 
-router.get('/add/pathalogy',function (req,res,next) {
-    var d=new Date();
+router.get('/add/pathalogy/:email',function (req,res,next) {
+
+
+    pathalogy.find({"email":req.params.email},function (err,docs) {
+
+        // res.json(docs.length)
+
+        if(docs.length<1) {
+
+            var d=new Date();
     var m=("0" + (d.getMonth() + 1)).slice(-2);
     var y=d.getFullYear();
     var d=("0" + (d.getDate() )).slice(-2);
 
     var item={
         "name":"asbkas",
-        "email":"",
+        "email":req.params.email,
         "date":y.toString()+m.toString()+d.toString(),
         "address":"",
         "lat":"",
@@ -340,39 +425,115 @@ router.get('/add/pathalogy',function (req,res,next) {
 
     })
 
+            res.send("insertion successfully");
+
+        }
+        else {
+            res.send("error")
+        }
+
+        // var  ty=json(docs.length)
+    });
 
 
-    res.send("insertion successfully");
 
 })
+
+
+
+
+
+
+
 
 
 ////////<////// now get allll ......>>
 /// get all doctor list
 
-router.get('get/docter/list',function (req,res,next) {
+router.get('/doctor/list',function (req,res,next) {
     doctor_search.find({ "search": "gogolio"},function (err,docs) {
       if(err) console.log(err)
        else {
-        res.json(docs[0])
+        res.json(docs[0].docters)
       }
     })
     //res.send("get sucessfully")
 })
 
-/// get all hospital
 
-router.get('get/docter/list',function (req,res,next) {
-    doctor_search.find({ "search": "gogolio"},function (err,docs) {
+//// get all hospital
+router.get('/hospital/list',function (req,res,next) {
+    hospital_search.find({ "search": "gogolio"},function (err,docs) {
         if(err) console.log(err)
         else {
-            res.json(docs[0])
+            res.json(docs[0].docters)
+        }
+    })
+    //res.send("get sucessfully")
+})
+
+//// get chemist list
+router.get('/chemist/list',function (req,res,next) {
+    chemist_search.find({ "search": "gogolio"},function (err,docs) {
+        if(err) console.log(err)
+        else {
+            res.json(docs[0].docters)
         }
     })
     //res.send("get sucessfully")
 })
 
 
+//// get ayurveda list
+router.get('/ayurveda/list',function (req,res,next) {
+    ayurveda_serach.find({ "search": "gogolio"},function (err,docs) {
+        if(err) console.log(err)
+        else {
+            res.json(docs[0].docters)
+        }
+    })
+    //res.send("get sucessfully")
+})
+
+//// pathalogy search
+router.get('/hospital/list',function (req,res,next) {
+    pathalogy_search.find({ "search": "gogolio"},function (err,docs) {
+        if(err) console.log(err)
+        else {
+            res.json(docs[0].docters)
+        }
+    })
+    //res.send("get sucessfully")
+})
+
+///homeolist
+router.get('/homeo/list',function (req,res,next) {
+    pathalogy_search.find({ "search": "gogolio"},function (err,docs) {
+        if(err) console.log(err)
+        else {
+            res.json(docs[0].docters)
+        }
+    })
+    //res.send("get sucessfully")
+})
+
+
+//// view your profile
+router.get('/be/doctor',function (req,res,next) {
+    doctor.find({},function (err,docs) {
+        if(err) console.log(err)
+        else {
+            res.json(docs[0].docters)
+        }
+    })
+    //res.send("get sucessfully")
+})
+
+
+
+
+
+//////////// Order a medicine
 
 
 
